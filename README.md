55f3518f1efd3dc3eb0ab983ad5ded0d2e40fe24
teagueStockwell <tsAppDevelopment@gmail.com>
2022-05-13 22:32:57 -0700

    Add rotor generators

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Enigma
+
+### a cypher implemented using a incremental stepping chain of substitution rotors
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,57 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+// ascii a-z
+export const charCodes = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122] as const
+const getCharCodes = () => Array.from({length: charCodes.length}, (_, i) => charCodes[i])
+export type CharCode = typeof charCodes[number]
+
+export type Rotor = {
+  // y: input, x: output
+  connections: Record<CharCode,CharCode>;
+  topDeadCenter: CharCode;
+}
+
+export const getPermutations = <T>(arr: T[], limit?: number): T[][] => {
+  const result: T[][] = [];
+
+  const permute = (arr: T[], m = [] as T[]) => {
+    if (arr.length === 0) {
+      result.push(m)
+      return
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      const curr = arr.slice();
+      const next = curr.splice(i, 1);
+      if (limit !== undefined && result.length !== limit) {
+        permute(curr.slice(), m.concat(next))
+      }
+    }
   }
-  return a + b;
-};
+
+ permute(arr)
+
+ return result;
+}
+
+const getHashCode = (s: string, range: number) => {
+  let acc = 0;
+  for(const c of s) {
+    acc += c.charCodeAt(0)
+  }
+  return Math.abs(acc % range)
+}
+
+const connectionPermutationsKeys = getPermutations(getCharCodes(), 100 * 1000)
+const connectionPermutationsValues = getPermutations(getCharCodes().reverse(), 100 * 1000)
+
+export const getRotorConnections = (hash: string): Record<CharCode,CharCode> => {
+  const keysIndex = getHashCode(hash, Math.floor(connectionPermutationsKeys.length / 4))
+  const valuesIndex = keysIndex * 4
+  const keys = connectionPermutationsKeys[keysIndex]
+  const values = connectionPermutationsValues[valuesIndex]
+  return keys.reduce((acc, key, i) => {
+    acc[key] = values[i]
+    return acc
+  }, {} as Record<CharCode,CharCode>)
+}
+
+export const getRotors = (hashes: string[]) => hashes.map(getRotorConnections)
