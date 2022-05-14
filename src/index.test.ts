0eae221b91ddea1352b7d8e7830e159ff4585d1c
teagueStockwell <tsAppDevelopment@gmail.com>
2022-05-14 01:12:38 -0700

    Simplify rotors

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,12 +1,23 @@
 import { getEnigma } from '.'
 
-it('makes rotors', () => {
-  const enigma = getEnigma('a'.repeat(10))
-  expect(enigma.rotors).toHaveLength(10)
+const enigma = getEnigma('a'.repeat(3))
+let res0: string
+let res1: string
+
+it('spins first rotor', () => {
+  res0 = enigma.encode('a'.repeat(2))
+  console.log(res0)
+  expect(enigma.state[0]).toBe('c')
 })
 
-it('spins rotors', () => {
-  const enigma = getEnigma('a'.repeat(10))
-  console.log(enigma.encode('a'.repeat(2)))
-  expect(enigma.state[0]).toBe('a')
+it('has a rolling shift', () => {
+  res1 = enigma.encode('a'.repeat(2))
+  console.log(res1)
+  expect(res1).not.toEqual(res0)
+})
+
+it('spins next rotor', () => {
+  const enigma2 = getEnigma('a'.repeat(3))
+  enigma2.encode('a'.repeat(24))
+  console.log(enigma2.state)
 })
--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,32 @@
-const rotor = {
-  'a': 'e',
-  'b': 'k',
-  'c': 'm',
-  'd': 'f',
-  'e': 'l',
-  'f': 'g',
-  'g': 'd',
-  'h': 'q',
-  'i': 'v',
-  'j': 'z',
-  'k': 'n',
-  'l': 't',
-  'm': 'o',
-  'n': 'w',
-  'o': 'y',
-  'p': 'h',
-  'q': 'x',
-  'r': 'u',
-  's': 's',
-  't': 'p',
-  'u': 'a',
-  'v': 'i',
-  'w': 'b',
-  'x': 'r',
-  'y': 'c',
-  'z': 'j',
-} as const
-
-type Char = keyof typeof rotor
-
-type Enigma = {
-  state: Char[],
-  rotors: typeof rotor[]
-  encode: (input: string) => string
-}
-
-const validateChars = (input: string) => {
-  if (input.split('').some(c => c.charAt(0) < 'a'.charAt(0) || c.charAt(0) > 'z'.charAt(0))) {
-    throw new Error('cypher must be a through z')
+const caesarShift = (char: string, amount: number):string => {
+  if (!char.length || char.length > 1) {
+    throw new Error('Invalid cesar shift');
   }
+
+  if (amount < 0) {
+    return caesarShift(char, amount + 26)
+  }
+
+  return String.fromCharCode(((char.charCodeAt(0) - 97 + amount) % 26) + 97)
 }
 
-export const getEnigma = (cypher: string): Enigma => {
-  validateChars(cypher)
-  const enigma: Enigma = {
-    state: cypher.split('') as Char[],
-    rotors: Array.from({length: cypher.length}, (_, i) => ({...rotor, topDeadCenter: cypher.charCodeAt(i)})),
+export const getEnigma = (cypher: string) => {
+  const enigma = {
+    state: cypher.split(''),
     encode: (input: string) => {
-      validateChars(input)
-      const {rotors, state} = enigma
+      const { state } = enigma
       let res = ''
-  
-      const spin = (topDeadCenter: string) => {
-        return topDeadCenter.charCodeAt(0) === 122 ? 'a' : String.fromCharCode(topDeadCenter.charCodeAt(0) + 1) as Char
-      }
-  
-      for (let i = 0; i < input.length; i++) {
-        const char = input[i] as Char
-        const firstKey = rotors[0][state[0]]
-        let encoded = state[0]
-        state[0] = spin(state[0])
-        for (let j = 1; j < rotors.length; j++) {
-          if (state[j - 1] === 'a') {
-            state[j] = spin(state[j])
+
+      const spin = (s: string) => s === 'z' ? 'a' : String.fromCharCode(s.charCodeAt(0) + 1)
+      
+      for (let charI = 0; charI < input.length; charI++) {
+        let encoded = input[charI]
+        state[0] = spin(state[0].charAt(0))
+        for (let rotorI = 1; rotorI < state.length; rotorI++) {
+          if (state[rotorI - 1] === 'a') {
+            state[rotorI] = spin(state[rotorI])
           }
-          encoded = rotors[j][encoded]
-          console.log({ encoded, char })
+          encoded = caesarShift(encoded, state[rotorI].charCodeAt(0) + 13)
         }
         res += encoded
       }
