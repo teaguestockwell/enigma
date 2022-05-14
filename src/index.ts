1bb24e65d612880b9bc01993437357b9c25192be
teagueStockwell <tsAppDevelopment@gmail.com>
2022-05-14 02:20:32 -0700

    Added more test

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,44 @@
 export const getEnigma = (cypher: string) => {
-  const enigma = {
-    state: cypher.split(''),
-    encode: (input: string) => {
-      const { state } = enigma
+  const e = {
+    state: cypher,
+    encode: (s: string) => {
+      const isInValid = (c: string) => c.charCodeAt(0) < 97 || c.charCodeAt(0) > 122
+      
+      for (const c of s) {
+        if (isInValid(c)) {
+          throw new Error('Invalid character in encode string')
+        }
+      }
+
+      for (const c of e.state) {
+        if (isInValid(c)) {
+          throw new Error('Invalid character in state string')
+        }
+      }
+
+
       let res = ''
 
-      for (const char of input) {
-        let encoded = char
+      for (const c of s) {
+        let encodeBuilder = c
 
-        for (let stateI = 0; stateI < state.length; stateI++) {
-          const next = state[stateI] === 'z' ? 'a' : String.fromCharCode(state[stateI].charCodeAt(0) + 1)
-          state[stateI] = next
+        for (let i = 0; i < e.state.length; i++) {
+          const next = e.state[i] === 'z' ? 'a' : String.fromCharCode(e.state[i].charCodeAt(0) + 1)
+          e.state = e.state.slice(0, i) + next + e.state.slice(i + 1)
           if (next !== 'a') {
             break
           }
         }
 
-        for (let rotorI = 0; rotorI < state.length; rotorI++) {
-          encoded = String.fromCharCode(((encoded.charCodeAt(0) - 97 + state[rotorI].charCodeAt(0) + 7) % 26) + 97)
+        for (let i = 0; i < e.state.length; i++) {
+          encodeBuilder = String.fromCharCode(((encodeBuilder.charCodeAt(0) - 97 + e.state[i].charCodeAt(0) + 7) % 26) + 97)
         }
 
-        res += encoded
+        res += encodeBuilder
       }
 
       return res
     }
   }
-  return enigma
+  return e
 }
